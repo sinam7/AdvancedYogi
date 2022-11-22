8bc8ed0fc7a5343154ee13ef7dc20192d232d746
SiNam7 <muwon777@naver.com>
2022-11-22 09:36:13 +0900

    git init

---
+++ src/main/java/project/sinam7/advancedYogi/advancedYogi/AdvancedYogiApplication.java
@@ -0,0 +1,13 @@
+package project.sinam7.advancedYogi.advancedYogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedYogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedYogiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/sinam7/advancedYogi/advancedYogi/AdvancedYogiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.sinam7.advancedYogi.advancedYogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedYogiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
