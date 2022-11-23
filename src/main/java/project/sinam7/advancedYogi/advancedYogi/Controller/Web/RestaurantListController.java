8384fbaba0484924e451376bea0587d683c15acb
SiNam7 <muwon777@naver.com>
2022-11-23 16:51:08 +0900

    n번째 페이지 정보까지 API call 가능

--- src/main/java/project/sinam7/advancedYogi/advancedYogi/Controller/Web/RestaurantListController.java
+++ src/main/java/project/sinam7/advancedYogi/advancedYogi/Controller/Web/RestaurantListController.java
@@ -1,22 +1,43 @@
 package project.sinam7.advancedYogi.advancedYogi.Controller.Web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import project.sinam7.advancedYogi.advancedYogi.Service.RestaurantService;
+import project.sinam7.advancedYogi.advancedYogi.Service.Secrets;
 
-@Controller
+@RestController // todo 나중에 페이지 만들면 Controller로
 @RequiredArgsConstructor
 @RequestMapping("/restaurants")
 public class RestaurantListController {
 
-    public final RestaurantService restaurantService;
+    private final RestaurantService restaurantService;
 
+    // TODO geo 정보 받아서 query param으로 받아 position 설정
     @GetMapping()
-    @ResponseBody
     public Object getRestaurants() {
+        setPosition(Secrets.MY_LAT, Secrets.MY_LNG);
         return restaurantService.getRestaurants();
     }
+
+    @GetMapping("/pages/{pageNum}")
+    public Object getRestaurants(@PathVariable String pageNum) {
+        setPosition(Secrets.MY_LAT, Secrets.MY_LNG);
+        return restaurantService.getRestaurants(Integer.parseInt(pageNum));
+    }
+
+    // 지금까지 받은 모든 식당 정보
+    @GetMapping("/loadAll")
+    public Object getAllLoadedRestaurants() {
+        return restaurantService.getAllRestaurants();
+    }
+
+    // todo 이거 이렇게 만들면 안될 것 같은데 어디로 옮기지
+    private void setPosition(double latitude, double longitude) {
+        restaurantService.setLatitude(latitude);
+        restaurantService.setLongitude(longitude);
+    }
+
+
 }
